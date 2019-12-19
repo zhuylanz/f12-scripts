d1fbbe0e584719cb46aa5e99c606f8e5569b9c6a
Lan Nguyen <zhuylanz@gmail.com>
2019-12-19 17:56:34 +0700

    ref: first commit

---
+++ facebook-invites.js
@@ -0,0 +1,48 @@
+async function run() {
+	let retries = 0;
+	while (retries < 3) {
+		await getAndClickInvites();
+		await loadMore();
+		if (!isMore()) {
+			retries += 1;
+		}
+	}
+}
+
+function isMore() {
+	const seeMoreButton = document.querySelectorAll(
+		"a.pam.uiBoxLightblue.uiMorePagerPrimary"
+	)[1];
+	if (seeMoreButton) {
+		return true;
+	}
+	return false;
+}
+
+async function getAndClickInvites() {
+	let inviteButtonArr = [];
+
+	document.querySelectorAll("a._42ft._4jy0._4jy3._517h._51sy").forEach(el => {
+		if (el.textContent == "Invite") {
+			inviteButtonArr.push(el);
+		}
+	});
+
+	for (let el of inviteButtonArr) {
+		el.click();
+		await sleep(1);
+	}
+}
+
+async function loadMore() {
+	document
+		.querySelectorAll("a.pam.uiBoxLightblue.uiMorePagerPrimary")[1]
+		.click();
+	await sleep(1);
+}
+
+function sleep(second) {
+	return new Promise(res => {
+		setTimeout(() => res(`slept ${second} second`), second * 1000);
+	});
+}
