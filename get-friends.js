351702566bd55d281c0d49a795485b598c036820
Lan Nguyen <zhuylanz@gmail.com>
2020-03-13 18:59:47 +0700

    ref: update get friends

--- get-friends.js
+++ get-friends.js
@@ -1,8 +1,12 @@
-const aTags = document.querySelectorAll(".clearfix._5qo4 .fsl.fwb.fcb a");
+var aTags = document.querySelectorAll(".clearfix._5qo4 .fsl.fwb.fcb a");
 for (let tag of aTags) {
 	console.log(tag.text);
 }
 
 for (let tag of aTags) {
-	console.log(tag.getAttribute("href"));
+	if (tag.getAttribute("href") != "#") {
+		console.log(JSON.parse(tag.getAttribute("data-gt")).engagement.eng_tid);
+	} else {
+		console.log("#");
+	}
 }
